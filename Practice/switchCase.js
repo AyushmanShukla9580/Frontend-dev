98c8984667c04dcd7e09cd05189e44afedbad6ca
ayushman <ayz@gmail.com>
2025-11-19 14:18:00 +0530

    file added

---
+++ Practice/switchCase.js
@@ -0,0 +1,36 @@
+let dayNumber = 5;
+let dayName;
+
+switch (dayNumber) {
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    case 4:
+        dayName = "Thursday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+    case 7:
+        dayName = "Sunday";
+        break;
+    default:
+        dayName = "Invalid day number";
+}
+
+console.log(`Today is ${dayName}.`);
+let result = "Hello" + 5;
+console.log(result);
+console.log(typeof result)
+let result1 = 10 - "4"
+console.log(result1)
+console.log(typeof result1)
